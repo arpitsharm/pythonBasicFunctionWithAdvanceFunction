ff05dcc6e168edb2ff0458e408f71602044844fe
arpitsharm <150226804+arpitsharm@users.noreply.github.com>
2024-01-30 18:46:19 +0530

    first commit

---
+++ func.py
@@ -0,0 +1,23 @@
+import os
+# os.rename("hello.txt", "buffalo.txt")
+
+# os.remove("buffalo.txt")
+
+# a = "hello boy"
+#
+# print(a.find("hellos"))
+
+# a = 12
+# print(a ==1)
+# print(a >1)
+# print(a >=1)
+# print(a ==12 or a < 1)
+
+
+
+
+
+
+
+
+
---
+++ pythonbasics.py
@@ -0,0 +1,83 @@
+# if elif else
+# a = int(input("What is Your Age :- "))
+# if a == 10:
+#     print("Your Are Gamer")
+# elif a > 100:
+#     print("You Are What?")
+#
+# elif a < 20:
+#     print("Sorry Moye-Moye!")
+#
+# else:
+#     print(f"{a} Age is not fear for this time")
+
+# loops
+
+# loops is infinity time running and else meaning it time for their for using here help you.
+
+# for loop
+
+# for loop is only for your times.
+
+# for i in range(4):
+#     print("Hello World")
+
+# a = "hello world"
+# for i in a:
+#     print(i, end="")
+
+# 1, 2, 3, 4 X
+# 1, 2, 3
+
+# while loop
+
+# while loop is infinite time
+
+# while True:
+#     print("hello")
+#
+# while False:
+#     print("hello")
+#
+# while None:
+#     print("hello")
+#
+# while 1:
+#     print("hello")
+
+
+# try to expect finally
+
+# try:
+#     a = "hdasnjdasjkdnask"
+#     print(a)
+#
+# except Exception as ex:
+#     print(f"Error is {ex}")
+#
+# finally:
+#     print("Hello")
+
+# 300
+# 50 variables -> store
+# function is implement to thinks in a one big codes tribute.
+
+# def forloop(time, text):
+#     for i in range(time):
+#         print(text, i+1)
+#
+# forloop(time=10, text="Hello Bhai Kaise Hoo")
+
+# def move(m1, m2):
+#     print(m2, m1)
+#
+# move(217398,  253)
+
+# def m():
+#     print("Hello")
+#
+# m()
+
+
+
+
